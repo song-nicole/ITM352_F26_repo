6a77fe10e2cce4508a89bbbbc56f3db9d4d1df02
song-nicole <songn@hawaii.edu>
2026-09-01 19:49:10 -1000

    Update expressions_tutorial.py
    Completed up to step 4

--- expressions_tutorial.py
+++ expressions_tutorial.py
@@ -1,7 +1,12 @@
 print(20 - 5 + 3)
 print(2 + 3 ** 2)
 
+name = "Nicole"
 age = 19
+hometown = "Honolulu"
+major = "Accounting"
+print("My name is ", name, ", I am from ", hometown, ", and I am currently majoring in ", major, ".")
+
 age_in_days = age * 365
 print("I have been alive for ", age_in_days, "days.")
 
@@ -33,3 +38,20 @@ print("I water my plants about ", yearly_watering, " times a year.")
 daily_cat_food_consumption = 0.8
 monthly_cat_food_consumption = daily_cat_food_consumption * 30
 print("My cats eat about ", monthly_cat_food_consumption, " cups of food a month.")
+
+professional_email_signature = name + " " +major + " " + expected_graduation_year
+
+address = "2500 Campus Road, Honolulu, HI 96822"
+address2 = '''2500 Campus Road,
+Honolulu, HI 96822'''
+
+initials = "N. S."
+birth_year = 2006
+social_media_username = initials + birth_year
+
+hobby1 = "felting"
+hobby2 = "gardening"
+hobby3 = "wire crafts"
+hobby4 = "jewelry making"
+goal = "graduate peacefully from college and find a fulfilling career in accounting"
+about_me = f"My name is {name}, I am {age} years old, and I am from {hometown}. My hobbies include a variety of activities, including {hobby1}, {hobby2}, {hobby3}, and {hobby4}. My current goal is simply to {goal}."
