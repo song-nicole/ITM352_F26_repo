9d1296d1bb2ffb739e8a8ee6541f1ec0ef82cad0
song-nicole <songn@hawaii.edu>
2026-09-01 19:24:58 -1000

    Create expressions_tutorial.py
    Completed parts 1 and 2

---
+++ expressions_tutorial.py
@@ -0,0 +1,35 @@
+print(20 - 5 + 3)
+print(2 + 3 ** 2)
+
+age = 19
+age_in_days = age * 365
+print("I have been alive for ", age_in_days, "days.")
+
+weekly_spending = 55
+monthly_expenses = weekly_spending * 52 / 12
+print("My monthly expenses total about $", monthly_expenses, ".")
+
+hours_alive = age * 365 * 24
+print("I have been alive for ", hours_alive, " hours.")
+
+gpa_4_point_scale = 3.97
+gpa_100_point_scale = gpa_4_point_scale * 25
+print("My GPA on a 100-point scale is ", gpa_100_point_scale, ".")
+
+years_before_100 = 100 - age
+print("I have ", years_before_100, " more years before I turn 100.")
+
+first_year_of_college = 2024
+this_year = 2026
+print("I have completed ", this_year - first_year_of_college, " years of college.")
+
+expected_graduation_year = first_year_of_college + 4
+print("I expect to graduate in ", expected_graduation_year, ".")
+
+weekly_watering = 3
+yearly_watering = weekly_watering * 52
+print("I water my plants about ", yearly_watering, " times a year.")
+
+daily_cat_food_consumption = 0.8
+monthly_cat_food_consumption = daily_cat_food_consumption * 30
+print("My cats eat about ", monthly_cat_food_consumption, " cups of food a month.")
