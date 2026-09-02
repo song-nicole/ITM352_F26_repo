3fc9d3fd3bc9d2a81e67958524e56579a07d62b8
song-nicole <songn@hawaii.edu>
2026-09-01 20:13:05 -1000

    Update expressions_tutorial.py

--- expressions_tutorial.py
+++ expressions_tutorial.py
@@ -55,3 +55,44 @@ hobby3 = "wire crafts"
 hobby4 = "jewelry making"
 goal = "graduate peacefully from college and find a fulfilling career in accounting"
 about_me = f"My name is {name}, I am {age} years old, and I am from {hometown}. My hobbies include a variety of activities, including {hobby1}, {hobby2}, {hobby3}, and {hobby4}. My current goal is simply to {goal}."
+
+print(age >= 18)
+print(age >= 21)
+print(age >= 25)
+print(gpa_4_point_scale >= 3.0)
+
+ics101 = True
+bus311 = True
+print(ics101 and bus311 and gpa_4_point_scale >= 3.0)
+
+class_standing = "Junior"
+print(class_standing and gpa_4_point_scale >= 3.0)
+
+budget = 250
+cost_of_laptop = 300
+cost_of_ipad = 170
+print(budget >= cost_of_laptop)
+print(budget >= cost_of_ipad)
+
+semester_tuition = 5880
+semester_fees = 457
+credit_hours = 18
+housing = 3000
+meals = 1052
+total_semester_cost = (semester_tuition + semester_fees) * credit_hours + (housing + meals)
+
+homework_avg = 98
+exam_avg = 93
+grade = (homework_avg * 0.3) + (exam_avg * 0.7)
+
+income = 9000
+rent = 2000
+utilities = 200
+groceries = 320
+entertainment = 28
+monthly_budget = income - (rent + utilities + groceries + entertainment)
+
+hours_per_week = 20
+number_of_courses = 6
+study_time = hours_per_week / number_of_courses
+print(study_time > 18)
