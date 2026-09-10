07d52ab5ff3b4015a4b757555e6b80b230b815c0
song-nicole <songn@hawaii.edu>
2026-09-09 16:44:39 -1000

    Lab 3.2a

---
+++ Lab3/Ex2a.py
@@ -0,0 +1,12 @@
+'''
+Create a function—call it midpoint —that takes two numbers as input and 
+returns the value halfway between them.
+'''
+#Name: Nicole Song
+#Date: Sept. 9, 2026
+
+
+def midpoint(num1, num2):
+    return (num1 + num2) / 2
+
+print(midpoint(10, 20))
