936f21f898f74654b35eda6a311e026e87039645
song-nicole <songn@hawaii.edu>
2026-09-11 12:59:40 -1000

    Create Ex3.py

---
+++ Lab3/Ex3.py
@@ -0,0 +1,12 @@
+'''
+Create a function called squareroot that takes a number 
+and returns the squareroot of that number. 
+'''
+
+#Name: Nicole Song
+#Date: Sept. 11, 2026
+
+def squareroot(num):
+    return num ** 0.5
+
+print(squareroot(4))
